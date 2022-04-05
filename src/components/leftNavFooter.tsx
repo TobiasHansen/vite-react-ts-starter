7c227be20704305781d1c5f52d8a001772c07d7e
TobiasHansen <hato1505@gmail.com>
2022-04-05 13:26:13 +0200

    Improve styling of LeftNavFooter

--- src/components/leftNavFooter.tsx
+++ src/components/leftNavFooter.tsx
@@ -1,30 +1,42 @@
-import { Avatar, Group, Navbar, Text } from '@mantine/core';
+import { Avatar, Box, Group, Navbar, Text, useMantineTheme } from '@mantine/core';
 
 import { SignInButton } from '~/components/signInButton';
 import { SignOutButton } from '~/components/signOutButton';
 import { useAuthState, useUser } from '~/contexts/authContext';
 
 export const LeftNavFooter = () => {
+  const theme = useMantineTheme();
   const { state } = useAuthState();
   const { user } = useUser();
 
   return (
     <Navbar.Section>
-      {state.state === 'UNKNOWN' ? null : state.state === 'SIGNED_OUT' ? (
-        <SignInButton />
-      ) : (
-        <>
-          <Group>
-            <Avatar size={40} color="blue">
-              {user?.displayName
-                ?.split(' ')
-                .map((s) => s[0])
-                .join('')}
-            </Avatar>
-            <Text>{user?.displayName ?? ''}</Text>
-            <SignOutButton />
+      {state.state !== 'UNKNOWN' && (
+        <Box
+          sx={{
+            paddingTop: theme.spacing.sm,
+            borderTop: `1px solid ${
+              theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+            }`,
+          }}
+        >
+          <Group position="center" spacing="xl">
+            {state.state === 'SIGNED_OUT' ? (
+              <SignInButton />
+            ) : (
+              <>
+                <Avatar size={40} color="blue">
+                  {user?.displayName
+                    ?.split(' ')
+                    .map((s) => s[0])
+                    .join('')}
+                </Avatar>
+                <Text>{user?.displayName ?? ''}</Text>
+                <SignOutButton />
+              </>
+            )}
           </Group>
-        </>
+        </Box>
       )}
     </Navbar.Section>
   );
